7fbc7972487dbe6bb3a53bb4651d25ec6421a267
Tarooba Faheem <tarooba.faheem.hussain@gmail.com>
2022-06-29 18:53:53 +0500

    Add some icons
    Add some icons
    setting
    menu
    exercise

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_svg/svg.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:sign_in/config.dart';
 import 'package:sign_in/controller/auth_controller.dart';
 import 'package:sign_in/widgets/hero_title.dart';
 import 'package:sign_in/widgets/rounded_elevated_button.dart';
 
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Center(child: GetBuilder<AuthController>(
@@ -18,8 +19,10 @@ class Home extends StatelessWidget {
             HeroTitle(
                 title:
                     'Hello ${_authController.displayName.toString().capitalizeString()}!',
-                subtitle: 'Let\'s being...'),
-            Expanded(flex: 8, child: Container()),
+                subtitle: 'Let\'s Start...'),
+            Expanded(flex: 8, child: Container(
+              )),
+            
             RoundedElevatedButton(
               title: 'Sign out',
               onPressed: () => _authController.signout(),
@@ -29,6 +32,8 @@ class Home extends StatelessWidget {
           ],
         );
       },
-    ));
+    ),
+    );
   }
 }
+
